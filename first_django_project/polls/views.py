720c243ef6fb5199c329504d4affbd183e965277
hasculdr <hasculdr@gmail.com>
2022-05-19 21:08:27 +0400

    повторил 8-е задание

--- first_django_project/polls/views.py
+++ first_django_project/polls/views.py
@@ -1,5 +1,28 @@
 from django.http import HttpResponse
+from django.http import Http404
+from django.template import loader
+from django.shortcuts import render, get_object_or_404
 
+from .models import Question
+
+#def index(request):
+#    return HttpResponse("Hello, world. 1a2d4440 is the polls index.")
+
+def owner(request):
+   return HttpResponse("Hello, world. 1a2d4440 is the polls index.")
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're vouting on question {question_id}.")
 
 def index(request):
-    return HttpResponse("Hello, world. 1a2d4440 is the polls index.")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list,}
+    return HttpResponse(template.render(context, request))
