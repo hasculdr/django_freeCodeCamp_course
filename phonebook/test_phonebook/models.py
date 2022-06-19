b9d5f2f7a32168b8f7a4642b575eab1bcff6f94f
hasculdr <hasculdr@gmail.com>
2022-06-19 13:19:43 +0400

    первый вариант базы, настройка панели админа

--- phonebook/test_phonebook/models.py
+++ phonebook/test_phonebook/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+class Peeps(models.Model):
+    phone_number = models.CharField("Номер телефона", max_length=11)
+    last_name = models.CharField("Фамилия", max_length=20)
+    first_name = models.ForeignKey("names", on_delete=models.SET_NULL, null=True)
+    middle_name = models.ForeignKey("patronymic", on_delete=models.SET_NULL, null=True)
+
+    class Meta:
+        verbose_name = "Фамилия"
+        verbose_name_plural = "Фамилии"
+
+    def __str__(self):
+        return self.last_name
+
+class Names(models.Model):
+    first_name = models.CharField("Имя", max_length=20)
+    
+    class Meta:
+        verbose_name = "Имя"
+        verbose_name_plural = "Имена"
+
+    def __str__(self):
+        return self.first_name
+
+class Patronymic(models.Model):
+    middle_name = models.CharField("Отчетсво", max_length=20)
+
+    class Meta:
+        verbose_name = "Отчетсво"
+        verbose_name_plural = "Отчества"
+
+    def __str__(self):
+        return self.middle_name
--- phonebook/test_phonebook/views.py
+++ phonebook/test_phonebook/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-#from django.http import HttpResponse
 
 def index(request):
-    return render(request, 'test_phonebook/index.html')
+    test = {'key1': [1, 2], 'key2': 'value2'}
+    return render(request, 'test_phonebook/index.html', test)
