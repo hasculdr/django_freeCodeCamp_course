abadd40271325aa1ff4efe93d02bdaf116dba282
Шепилов Семён Николаевич <hasculdr@gmail.com>
2022-09-02 20:59:00 +0400

    откат хотелок автогрейдера

--- first_django_project/autos/models.py
+++ first_django_project/autos/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(("Manufacturer"), max_length=50)
+    name = models.CharField(("Maker"), max_length=50)
 
     def __str__(self):
         return self.name
@@ -12,7 +12,7 @@ class Auto(models.Model):
     nickname = models.CharField(("Nickname"), max_length=20)
     mileage = models.IntegerField(("Milleage"), default=0)
     comments = models.CharField(("Comments"), max_length=50)
-    auto = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=False)
+    make = models.ForeignKey(Manufacturer, on_delete=models.CASCADE, null=False)
 
     def __str__(self):
         return self.nickname
