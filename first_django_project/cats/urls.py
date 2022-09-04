949486c31d8f121759f3685e531b8389fab4e939
Шепилов Семён Николаевич <hasculdr@gmail.com>
2022-09-04 22:28:22 +0400

    изменен формат ссылок

--- first_django_project/cats/urls.py
+++ first_django_project/cats/urls.py
@@ -5,11 +5,11 @@ app_name = 'cats'
 urlpatterns = [
     path('', views.CatsPage.as_view(), name='cats_page'),
     path('breeds/', views.BreedsPage.as_view(), name='breeds_page'),
-    path('breed_create/', views.CreateBreed.as_view(), name='create_breed'),
-    path('breed_update/<int:pk>/', views.UpdateBreed.as_view(), name='update_breed'),
-    path('breed_delete/<int:pk>/', views.DeleteBreed.as_view(), name='delete_breed'),
-    path('cat_create/', views.CreateCat.as_view(), name='create_cat'),
-    path('cat_update/<int:pk>/', views.UpdateCat.as_view(), name='update_cat'),
-    path('cat_delete/<int:pk>', views.DeleteCat.as_view(), name='delete_cat'),
+    path('create/breed/', views.CreateBreed.as_view(), name='create_breed'),
+    path('update/breed/<int:pk>/', views.UpdateBreed.as_view(), name='update_breed'),
+    path('delete/breed/<int:pk>/', views.DeleteBreed.as_view(), name='delete_breed'),
+    path('create/cat/', views.CreateCat.as_view(), name='create_cat'),
+    path('update/cat/<int:pk>/', views.UpdateCat.as_view(), name='update_cat'),
+    path('delete/cat/<int:pk>', views.DeleteCat.as_view(), name='delete_cat'),
 ]
 
