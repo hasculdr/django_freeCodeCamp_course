d7d61bf765ffc558093794c729331053b9dd701f
Шепилов Семён Николаевич <hasculdr@gmail.com>
2022-09-19 21:11:01 +0400

    протыкано задание 26, уже без понимания

--- first_django_project/ads/views.py
+++ first_django_project/ads/views.py
@@ -1,11 +1,11 @@
 from django.views import View
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect, get_object_or_404, reverse
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from django.contrib.auth.mixins import LoginRequiredMixin
 
-from ads.models import Ad
-from ads.forms import CreateForm
+from ads.models import Ad, Comment
+from ads.forms import CreateForm, CommentForm
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
 
 
@@ -18,7 +18,15 @@ class AdListView(OwnerListView):
 
 class AdDetailView(OwnerDetailView):
     model = Ad
-    template_name = 'ads/detail.html'
+    template_name = 'ads/ad_detail.html'
+
+    def get(self, request, pk) :
+        x = Ad.objects.get(id=pk)
+        comments = Comment.objects.filter(ad=x).order_by('-updated_at')
+        comment_form = CommentForm()
+        context = { 'ad' : x, 'comments': comments, 'comment_form': comment_form }
+        return render(request, self.template_name, context)
+
 
 class AdCreateView(LoginRequiredMixin, View):
     template_name='ads/form.html'
@@ -74,3 +82,20 @@ def stream_file(request, pk):
     response['Content-Length'] = len(pic.picture)
     response.write(pic.picture)
     return response
+
+
+class CommentCreateView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        f = get_object_or_404(Ad, id=pk)
+        comment = Comment(text=request.POST['comment'], owner=request.user, ad=f)
+        comment.save()
+        return redirect(reverse('ads:ad_detail', args=[pk]))
+
+class CommentDeleteView(OwnerDeleteView):
+    model = Comment
+    template_name = "ads/comment_delete.html"
+
+    # https://stackoverflow.com/questions/26290415/deleteview-with-a-dynamic-success-url-dependent-on-id
+    def get_success_url(self):
+        ad = self.object.ad
+        return reverse('ads:ad_detail', args=[ad.id])
